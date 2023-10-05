9d81287f2ce69f907704f4d4549cdc8f120b7eb6
Ian0001 <ianvrdoljak@hotmail.es>
2023-10-05 09:17:47 -0300

    Agregué nodemailer para notificaciones por email

---
+++ src/controllers/sendEmailNotification.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+const {NODEMAILER_ACCOUNT_PASSWORD, NODEMAILER_ACCOUNT} = process.env;
+
+const sendEmailNotification = async (emailAddress, message, subject) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: `${NODEMAILER_ACCOUNT}`,
+                pass: `${NODEMAILER_ACCOUNT_PASSWORD}`
+            }
+        });
+
+        const mailConfig = {
+            from: "",
+            to: emailAddress,
+            subject: subject,
+            text: message
+        };
+
+        const info = await transporter.sendMail(mailConfig);
+        console.log("Email sent: " + info.response);
+
+    } catch (error) {
+        console.error(error.message);
+        throw new Error("Ocurrió un error");
+    }
+};
